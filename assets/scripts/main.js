72d9fb097d67537d56e6e7b812c78d596ea4740d
Godstime Onyibe <godstimeonyibe@gmail.com>
2020-12-03 16:07:39 +0100

    Modified nav and make it fixed on a scroll

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,17 @@
 // ````````````````date````````````````
 const now = new Date().getFullYear()
 document.querySelector('#date').innerHTML = now
+
+// ````````````fix nav ```````````````````
+const nav = document.querySelector('.header-nav')
+const topOfNav = nav.offsetTop;
+const fixNav = () =>{
+  if (window.scrollY == topOfNav) {
+    document.body.style.paddingTop = 0;
+    document.body.classList.add('header-list')
+    document.body.classList.remove('fixedNav')
+  } else {
+    document.body.classList.add('fixedNav')
+  }
+}
+window.addEventListener('scroll', fixNav)
